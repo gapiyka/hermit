c51f5b59ffc56b49c88fcfd62810e3103105c30a
gapiyka <50524296+p-pelmeni@users.noreply.github.com>
2021-03-02 17:41:35 +0200

    game_start + more_enemies

--- js/game.js
+++ js/game.js
@@ -95,11 +95,8 @@ class InputHandler {
                 case 83://S
                     player.moveDown();
                     break;
-                case 27://esc
-                    alert("PAUSE");
-                    break;
                 case 32://space
-                    alert("START");
+                    if (!IsGameStarted) StartGame();
                     break;
             }
         });
@@ -142,25 +139,42 @@ class Enemy {
         this.height = 40;
         this.maxSpeed = 3;
         this.speed = { x: 0, y: 0 };
-        const spawn = randomSpawn(this.width, this.height);
+        const spawn = randomSnakeSpawn(this.width, this.height);
         this.degree = spawn.degree;
         this.side = spawn.side;
         this.position = {
             x: spawn.x,
             y: spawn.y
         };
+        this.colision = {};
         switch (this.side) {
             case "top":
                 this.speed.y = this.maxSpeed;
+                this.colision = {
+                    x: this.position.x - this.width,
+                    y: this.position.y - this.height
+                }
                 break;
             case "right":
                 this.speed.x = -this.maxSpeed;
+                this.colision = {
+                    x: this.position.x,
+                    y: this.position.y - this.height
+                }
                 break;
             case "bottom":
                 this.speed.y = -this.maxSpeed;
+                this.colision = {
+                    x: this.position.x,
+                    y: this.position.y,
+                }
                 break;
             case "left":
                 this.speed.x = this.maxSpeed;
+                this.colision = {
+                    x: this.position.x - this.width,
+                    y: this.position.y,
+                }
                 break;
         }
     }
@@ -176,6 +190,8 @@ class Enemy {
         if (!deltaTime) return;
         this.position.x += this.speed.x;
         this.position.y += this.speed.y;
+        this.colision.x += this.speed.x;
+        this.colision.y += this.speed.y;
     }
 }
 
@@ -192,9 +208,11 @@ const GAME_HEIGHT = 600;
 const imghero = document.getElementById("hero");
 const imgsnake = document.getElementById("snake");
 
-let player = new Player(GAME_WIDTH, GAME_HEIGHT, imghero);
-let snakeEnemy = new Snake(imgsnake);
+let player = new Player(GAME_WIDTH, GAME_HEIGHT, imghero);;
+let Hendler;
+let snakeEnemys;
 let lastTime = 0;
+let IsGameStarted = false;
 
 
 //FUNCTIONS:
@@ -202,30 +220,30 @@ function random(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
 
-function randomSpawn(objWidth, objHeight) {//generate spawn position+side of enemy(snake)
+function randomSnakeSpawn(objWidth, objHeight) {//generate spawn position+side of enemy(snake)
     const sides = [//0-top 1-right 2-bottom 3-left
         {
             side: "top",
-            x: random(0, GAME_WIDTH - objWidth),
+            x: random(objWidth, GAME_WIDTH - objWidth),
             y: 0,
             degree: 180
         },
         {
             side: "right",
             x: GAME_WIDTH,
-            y: random(0, GAME_HEIGHT - objHeight),
+            y: random(objHeight, GAME_HEIGHT - objHeight),
             degree: 270
         },
         {
             side: "bottom",
-            x: random(0, GAME_WIDTH - objWidth),
+            x: random(objWidth, GAME_WIDTH - objWidth),
             y: GAME_HEIGHT,
             degree: 0
         },
         {
             side: "left",
             x: 0,
-            y: random(0, GAME_HEIGHT - objHeight),
+            y: random(objHeight, GAME_HEIGHT - objHeight),
             degree: 90
         }
     ];
@@ -233,23 +251,52 @@ function randomSpawn(objWidth, objHeight) {//generate spawn position+side of ene
 }
 
 function gameLoop(timestamp) {
-    let deltaTime = timestamp - lastTime;
-    lastTime = timestamp;
+    if (IsGameStarted) {
+        let deltaTime = timestamp - lastTime;
+        lastTime = timestamp;
 
-    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-    player.update(deltaTime);
-    player.draw(ctx);
-    if ((snakeEnemy.position.x > -100 && snakeEnemy.position.x < GAME_WIDTH + 100) && (snakeEnemy.position.y > -100 && snakeEnemy.position.y < GAME_HEIGHT + 100)) {
-        snakeEnemy.update(deltaTime);
-        snakeEnemy.draw(ctx);
+        ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        player.update(deltaTime);
+        player.draw(ctx);
+        snakeEnemys.forEach(snakeEnemy => {
+            //check on existing in borders
+            if ((snakeEnemy.position.x > -100 && snakeEnemy.position.x < GAME_WIDTH + 100) && (snakeEnemy.position.y > -100 && snakeEnemy.position.y < GAME_HEIGHT + 100)) {
+                snakeEnemy.update(deltaTime);
+                snakeEnemy.draw(ctx);
+                //collision
+                if ((player.position.x + player.width >= snakeEnemy.colision.x && player.position.x + player.width <= snakeEnemy.colision.x + snakeEnemy.width) || (player.position.x <= snakeEnemy.colision.x + snakeEnemy.width && player.position.x >= snakeEnemy.colision.x)) {
+                    if ((player.position.y + player.height >= snakeEnemy.colision.y && player.position.y + player.height <= snakeEnemy.colision.y + snakeEnemy.height) || (player.position.y <= snakeEnemy.colision.y + snakeEnemy.height && player.position.y >= snakeEnemy.colision.y)) {
+                        IsGameStarted = false;
+                    }
+                }
+            }
+            else {
+                if (snakeEnemys.indexOf(snakeEnemy) == 1) snakeEnemys.pop();
+                else snakeEnemys.shift();//' snakeEnemy = new Snake(imgsnake) ' doesn`t work
+                snakeEnemys.push(new Snake(imgsnake));
+            }
+        });
     }
-    else snakeEnemy = new Snake(imgsnake);
+    else {
+        ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        player.draw(ctx);
+        ctx.font = `48px serif`;
+        ctx.fillText("Press 'Space' to start game", GAME_WIDTH / 6, GAME_HEIGHT / 6);
+    }
 
     requestAnimationFrame(gameLoop);//https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
 }
 
+function StartGame() {
+    player = new Player(GAME_WIDTH, GAME_HEIGHT, imghero);
+    snakeEnemys = [];
+    snakeEnemys.push(new Snake(imgsnake));
+    snakeEnemys.push(new Snake(imgsnake));
+    IsGameStarted = true;
+    Hendler = new InputHandler(player);
+}
+
 
 //LAUNCH
-new InputHandler(player);
-player.draw(ctx);
+Hendler = new InputHandler(player);
 gameLoop();
